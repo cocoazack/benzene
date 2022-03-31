2a8b5c08c52f22e412e7c0716c9fb818080c528f
z <givbw@hotmail.com>
2022-03-31 22:58:38 +0800

    ci

---
+++ src/layout/MainFooter.vue
@@ -0,0 +1,55 @@
+<template>
+  <footer
+    class="footer"
+    :class="{ [`footer-${type}`]: type }"
+    :data-background-color="backgroundColor"
+  >
+    <div class="container">
+      <nav>
+        <ul>
+          <!--
+          <li>
+            <a href="https://www.creative-tim.com">
+              Creative Tim
+            </a>
+          </li>
+          -->
+          <li>
+            <a href="">
+              关于我们
+            </a>
+          </li>
+          <li>
+            <a href="https://encoderlee.blog.csdn.net/">
+              博客
+            </a>
+          </li>
+          <!--
+          <li>
+            <a href="https://www.creative-tim.com/license">
+              Licenses
+            </a>
+          </li>
+          -->
+        </ul>
+      </nav>
+      <div class="copyright">
+        <p>开发元宇宙链游自动化脚本服务<br>版权所有 &copy; {{ year }}</p>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  props: {
+    backgroundColor: String,
+    type: String
+  },
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+<style></style>
---
+++ src/views/Landing.vue
@@ -0,0 +1,418 @@
+<template>
+  <div class="wrapper">
+    <parallax class="section page-header header-filter" :style="headerStyle">
+      <div class="container">
+        <div class="md-layout">
+          <div
+            class="md-layout-item md-size-50 md-small-size-70 md-xsmall-size-100"
+          >
+            <h1 class="title">链游脚本世界</h1>
+            <h4>
+              一个透明，公开，安全的链游自动化脚本服务平台，值得信赖。
+            </h4>
+            <br />
+            <md-button
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              class="md-success md-lg"
+              target="_blank"
+              > 开始使用</md-button
+            >
+          </div>
+        </div>
+      </div>
+    </parallax>
+    <div class="main main-raised">
+      <div class="section">
+        <div class="container">
+          <div class="md-layout">
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="metaImg1"
+                        alt="Thumbnail Image"
+                        class="img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      什么是元宇宙
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        是利用科技手段进行链接与创造的，与现实世界映射与交互的虚拟世界
+                      </p>
+                    </md-card-content>
+                  </md-card>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="metaImg2"
+                        alt="Thumbnail Image"
+                        class="img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      元宇宙的前景
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        元宇宙或是工业和互联网的下一次大变革方向。
+                      </p>
+                    </md-card-content>
+                  </md-card>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="metaImg3"
+                        alt="Thumbnail Image"
+                        class="img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      元宇宙为何被需求
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        它可给人们生活带来效率提升，成本降低
+                      </p>
+                    </md-card-content>
+                  </md-card>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="metaImg4"
+                        alt="Thumbnail Image"
+                        class="img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      元宇宙游戏价值
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        在元宇宙游戏中可以创造具有流转价值的资产
+                      </p>
+                    </md-card-content>
+                  </md-card>
+              </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="md-layout">
+            <div
+              class="md-layout-item md-size-66 md-xsmall-size-100 mx-auto text-center"
+            >
+              <h2 class="title text-center">我们服务的优势</h2>
+            </div>
+          </div>
+          <div class="features text-center">
+            <div class="md-layout">
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="info">
+                  <div class="icon icon-info">
+                    <md-icon>public</md-icon>
+                  </div>
+                  <h4 class="info-title">透明</h4>
+                  <p>
+                    游戏在前端运行，自动化操作，每一步都可以看到
+                  </p>
+                </div>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="info">
+                  <div class="icon icon-primary">
+                    <md-icon>emoji_emotions</md-icon>
+                  </div>
+                  <h4 class="info-title">易用</h4>
+                  <p>
+                    一键操作，无人值守，省心省力
+                  </p>
+                </div>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="info">
+                  <div class="icon icon-success">
+                    <md-icon>privacy_tip</md-icon>
+                  </div>
+                  <h4 class="info-title">安全</h4>
+                  <p>
+                    无须用户账户、密码、私钥等隐私信息，在浏览器内运行，运行环境和系统隔离
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--
+      <div class="section text-center">
+        <div class="container">
+          <h2 class="title">Here is our team</h2>
+          <div class="team">
+            <div class="md-layout">
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="team-player">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="teamImg1"
+                        alt="Thumbnail Image"
+                        class="img-raised rounded-circle img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      Gigi Hadid
+                      <br />
+                      <small class="card-description text-muted">Model</small>
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        You can write here details about one of your team
+                        members. You can give more details about what they do.
+                        Feel free to add some <a href="#">links</a> for people
+                        to be able to follow them outside the site.
+                      </p>
+                    </md-card-content>
+
+                    <md-card-actions class="text-center">
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-twitter"></i>
+                      </md-button>
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-instagram"></i>
+                      </md-button>
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-facebook-square"></i>
+                      </md-button>
+                    </md-card-actions>
+                  </md-card>
+                </div>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="team-player">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="teamImg2"
+                        alt="Thumbnail Image"
+                        class="img-raised rounded-circle img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      Carla Hortensia
+                      <br />
+                      <small class="card-description text-muted"
+                        >Designer</small
+                      >
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        You can write here details about one of your team
+                        members. You can give more details about what they do.
+                        Feel free to add some <a href="#">links</a> for people
+                        to be able to follow them outside the site.
+                      </p>
+                    </md-card-content>
+
+                    <md-card-actions class="text-center">
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-twitter"></i>
+                      </md-button>
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-linkedin"></i>
+                      </md-button>
+                    </md-card-actions>
+                  </md-card>
+                </div>
+              </div>
+              <div class="md-layout-item md-medium-size-33 md-small-size-100">
+                <div class="team-player">
+                  <md-card class="md-card-plain">
+                    <div class="md-layout-item md-size-50 mx-auto">
+                      <img
+                        :src="teamImg3"
+                        alt="Thumbnail Image"
+                        class="img-raised rounded-circle img-fluid"
+                      />
+                    </div>
+                    <h4 class="card-title">
+                      Kendall Jenner
+                      <br />
+                      <small class="card-description text-muted">Model</small>
+                    </h4>
+
+                    <md-card-content>
+                      <p class="card-description">
+                        You can write here details about one of your team
+                        members. You can give more details about what they do.
+                        Feel free to add some <a href="#">links</a> for people
+                        to be able to follow them outside the site.
+                      </p>
+                    </md-card-content>
+
+                    <md-card-actions class="text-center">
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-twitter"></i>
+                      </md-button>
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-instagram"></i>
+                      </md-button>
+                      <md-button
+                        href="javascript:void(0)"
+                        class="md-just-icon md-simple"
+                      >
+                        <i class="fab fa-facebook-square"></i>
+                      </md-button>
+                    </md-card-actions>
+                  </md-card>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section section-contacts">
+        <div class="container">
+          <div class="md-layout">
+            <div class="md-layout-item md-size-66 md-xsmall-size-100 mx-auto">
+              <h2 class="text-center title">Work with us</h2>
+              <h4 class="text-center description">
+                Divide details about your product or agency work into parts.
+                Write a few lines about each one and contact us about any
+                further collaboration. We will responde get back to you in a
+                couple of hours.
+              </h4>
+              <form class="contact-form">
+                <div class="md-layout">
+                  <div class="md-layout-item md-size-50">
+                    <md-field>
+                      <label>Your Name</label>
+                      <md-input v-model="name" type="text"></md-input>
+                    </md-field>
+                  </div>
+                  <div class="md-layout-item md-size-50">
+                    <md-field>
+                      <label>Your Email</label>
+                      <md-input v-model="email" type="email"></md-input>
+                    </md-field>
+                  </div>
+                </div>
+                <md-field maxlength="5">
+                  <label>Your Message</label>
+                  <md-textarea v-model="message"></md-textarea>
+                </md-field>
+                <div class="md-layout">
+                  <div class="md-layout-item md-size-33 mx-auto text-center">
+                    <md-button class="md-success">Send Message</md-button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  bodyClass: "landing-page",
+  props: {
+    header: {
+      type: String,
+      default: require("@/assets/img/bg7.jpg")
+    },
+    metaImg1: {
+      type: String,
+      default: require("@/assets/img/meta/metaverse.png")
+    },
+    metaImg2: {
+      type: String,
+      default: require("@/assets/img/meta/future.jpg")
+    },
+    metaImg3: {
+      type: String,
+      default: require("@/assets/img/meta/requiement.jpg")
+    },
+    metaImg4: {
+      type: String,
+      default: require("@/assets/img/meta/worth.jpg")
+    },
+    teamImg1: {
+      type: String,
+      default: require("@/assets/img/faces/avatar.jpg")
+    },
+    teamImg2: {
+      type: String,
+      default: require("@/assets/img/faces/christian.jpg")
+    },
+    teamImg3: {
+      type: String,
+      default: require("@/assets/img/faces/kendall.jpg")
+    }
+  },
+  data() {
+    return {
+      name: null,
+      email: null,
+      message: null
+    };
+  },
+  computed: {
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.header})`
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.md-card-actions.text-center {
+  display: flex;
+  justify-content: center !important;
+}
+.contact-form {
+  margin-top: 30px;
+}
+
+.md-has-textarea + .md-layout {
+  margin-top: 15px;
+}
+</style>
